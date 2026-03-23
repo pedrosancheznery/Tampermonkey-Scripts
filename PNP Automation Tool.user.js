be41ce488fcfce830d0d09c82d69e6a274e48f5a
Pedro Sánchez Nery <pedro.sanchez.nery@gmail.com>
2026-03-23 01:02:12 -0500

    Bump version from 1.4.3 to 1.4.4

--- PNP Automation Tool.user.js
+++ PNP Automation Tool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PNP Automation Tool
 // @namespace    HOU3
-// @version      1.4.3
+// @version      1.4.4
 // @description  Automate palletization by processing a list of IDs via the PNP tool logic; logs unprocessed totes and continues on error modals
 // @author       Pedro Sanchez (pefsanch)
 // @match        https://pnp-iad.aka.amazon.com/pnp
@@ -297,9 +297,11 @@
             if (h4) reason = h4.textContent.trim();
             reason.match('Error\s+is\b[^:]*:\s*(Container is empty)');
             const match = reason.match(regex);
-            if (match ) {console.log(match);}
-            realReason = match[1];
-            reason = realReason;
+            if ( match ) {
+                console.log('MAtched:' , match);
+                realReason = match[1];
+                reason = realReason;
+            }
             const headerDiv = modalEl.querySelector('.errorMessageHeader');
             if (!reason && headerDiv) reason = headerDiv.textContent.trim();
             const nextStep = modalEl.querySelector('.errorMessageNextStep');
