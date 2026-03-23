90351bb823c45a5438af77cabf29c392b8dd970d
Pedro Sánchez Nery <pedro.sanchez.nery@gmail.com>
2026-03-23 02:13:56 -0500

    PNP Automation Tool Update version number and fix typo in condition

--- PNP Automation Tool.user.js
+++ PNP Automation Tool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PNP Automation Tool
 // @namespace    HOU3
-// @version      1.4.8.12
+// @version      1.4.8.14
 // @description  Automate palletization by processing a list of IDs via the PNP tool logic; logs unprocessed totes and continues on error modals
 // @author       Pedro Sanchez (pefsanch)
 // @match        https://pnp-iad.aka.amazon.com/pnp
@@ -303,7 +303,7 @@
             if (h4) reason = h4.textContent.trim();
             //reason.match(emptyregex);
             let match = reason.match(emptyregex);
-            if ( martch && match[1] != '' ) {
+            if ( match && match[1] != '' ) {
                 console.log('Matched:' , match[1]);
                 realReason = match[1];
                 reason = realReason;
