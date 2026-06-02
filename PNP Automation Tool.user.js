d6752c5738bb0a8633f0d717126870b42549c00c
Pedro Sánchez Nery <pedro.sanchez.nery@gmail.com>
2026-06-01 23:22:32 -0500

    Add new match URL for PNP web app

--- PNP Automation Tool.user.js
+++ PNP Automation Tool.user.js
@@ -5,6 +5,7 @@
 // @description  Automate palletization by processing a list of IDs via the PNP tool logic; logs unprocessed totes and continues on error modals
 // @author       Pedro Sanchez (pefsanch)
 // @match        https://pnp-iad.aka.amazon.com/pnp*
+// @match        https://pnpwebapp.na.aftx.amazonoperations.app/pnp*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.com
 // @homepage     https://github.com/pedrosancheznery/Tampermonkey-Scripts
 // @downloadURL  https://raw.githubusercontent.com/pedrosancheznery/Tampermonkey-Scripts/main/PNP%20Automation%20Tool.user.js
